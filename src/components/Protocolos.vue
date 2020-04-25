1d9784acd515c72fe67fe2a7cb32cd819bfd394b
dvillalobos16 <d_villalobos@outlook.com>
2020-04-25 10:02:06 -0600

    Activacion de boton protocolo completo y correcciones vitamina C

--- src/components/Protocolos.vue
+++ src/components/Protocolos.vue
@@ -39,17 +39,18 @@
                         <div class="mr-5">
                             <h1 class="font-black text-base uppercase">{{protocolos.phase1.p2.name}}</h1>
                             <ul class="mt-1 mb-2 flex flex-col justify-center items-start list-none text-xs font-medium text-red-600">
-                                <li class="leading-normal">Se recomienda que no utilice más de 2mg diarios.</li>
-                                <li class="leading-normal">No usar en niños menores de 12 años.</li>
-                                <li class="leading-normal">No use este producto si tiene problemas con los riñones.</li>
-                                <li class="leading-normal">No puede ingerir Vitamina B17 con el uso de este suplemento.</li>
+                                <li class="leading-normal">*Se recomienda que no utilice más de 2mg diarios.</li>
+                                <li class="leading-normal">*No usar en niños menores de 12 años.</li>
+                                <li class="leading-normal">*No use este producto si tiene problemas con los riñones.</li>
+                                <li class="leading-normal">*No puede ingerir Vitamina B17 con el uso de este suplemento.</li>
+                                <li class="leading-normal">*Las declaraciones sobre los suplementos dietéticos no han sido evaluadas por la FDA y no están destinadas a diagnosticar, tratar, curar o prevenir ninguna enfermedad o problema de salud.</li>
                             </ul>
-                            <p class="font-normal text-sm text-gray-700 max-w-xs leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase1.p2.dose}}</span></p>
+                            <p class="font-normal text-base text-gray-700 leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase1.p2.dose}}</span></p>
                             <!--<p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>-->
-                            <p class="font-black text-2xl text-black pb-0 pt-1">${{protocolos.phase1.p2.price}}</p>
+                            <p class="font-black text-3xl text-black pb-0 pt-1">${{protocolos.phase1.p2.price}}</p>
                         </div>
                         <!--<p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>-->
-                        <a :href="protocolos.phase1.p2.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-sm bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
+                        <a :href="protocolos.phase1.p2.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-base bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-4 py-4 lg:px-4 lg:py-3 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
                     </div>
                     <p class="w-full lg:w-4/5 text-sm mb-3 text-black mt-2 leading-tight"><span class="font-black">Importante:</span> Esta compra cubrirá un período de 120 días dentro de los 180 días sugeridos para este protocolo.</p>
                 </div>
@@ -61,17 +62,17 @@
                             <img src="../assets/wound-fixer.jpeg" class="w-24 mr-2"/>
                             <div class="mr-5">
                                 <h1 class="font-black text-base uppercase">{{protocolos.phase1.p3.name}}</h1>
-                                <p class="font-normal text-sm text-gray-700 max-w-xs leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase1.p3.dose}}</span></p>
-                                <p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>
-                                <!--<p class="font-black text-2xl text-black pb-0 pt-1">${{protocolos.phase1.p3.price}}</p>-->
+                                <p class="font-normal text-base text-gray-700 leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase1.p3.dose}}</span></p>
+                                <!--<p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>-->
+                                <p class="font-black text-3xl text-black pb-0 pt-1">${{protocolos.phase1.p3.price}}</p>
                             </div>
-                            <p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>
-                            <!--<a :href="protocolos.phase1.p3.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-sm bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>-->
+                            <!--<p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>-->
+                            <a :href="protocolos.phase1.p3.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-base bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-4 py-4 lg:px-4 lg:py-3 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
                         </div>
                         <p class="w-full lg:w-4/5 text-sm mb-3 text-black mt-2 leading-tight"><span class="font-black">Importante:</span> Esta compra cubrirá un período de 30 días dentro de los {{protocolos.phase1.duration}} sugeridos para este protocolo.</p>
                     </div>
                     <!--<p class="flex flex-row flex-wrap justify-center items-center text-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-2 px-2 rounded-lg mb-5 mt-5 hover:shadow-lg transition-all font-normal text-xl focus:outline-none"><i class="ri-error-warning-line mr-3 text-xl lg:text-2xl"></i>Por el momento no esta disponible el protocolo completo, intenta comprar ImmuneBioGreenCell individualmente y recibe un 30% de descuento <br><i class="ri-home-heart-fill mr-3 text-xl lg:text-2xl"></i> #QuedateEnCasa</p>-->
-                <a :href="protocolos.phase1.linkPhase1" class="flex flex-row flex-wrap justify-center items-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-4 rounded-lg mb-5 mt-5 hover:shadow-lg hover:bg-black transition-all font-normal text-xl focus:outline-none"><i class="ri-shopping-cart-2-fill mr-3 text-xl lg:text-2xl"></i>Comprar protocolo completo<span class="ml-2 font-bold text-2xl text-center lg:text-lg lg:text-left w-full lg:w-auto lg:text-lg">${{price1()}}</span></a>
+                <a :href="protocolos.phase1.linkPhase1" class="flex flex-row flex-wrap justify-center items-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-4 rounded-lg mb-5 mt-5 hover:shadow-lg hover:bg-black transition-all font-normal text-xl focus:outline-none"><i class="ri-shopping-cart-2-fill mr-3 text-xl lg:text-2xl"></i>Comprar protocolo completo<span class="ml-2 font-bold text-3xl text-center lg:text-lg lg:text-left w-full lg:w-auto lg:text-3xl">${{price1()}}</span></a>
             </div>
             <!--Aqui finaliza la fase 1-->
             <!--Aqui inicia la fase 2-->
@@ -106,17 +107,18 @@
                         <div class="mr-5">
                             <h1 class="font-black text-base uppercase">{{protocolos.phase2.p2.name}}</h1>
                             <ul class="mt-1 mb-2 flex flex-col justify-center items-start list-none text-xs font-medium text-red-600">
-                                <li class="leading-normal">Se recomienda que no utilice más de 2mg diarios.</li>
-                                <li class="leading-normal">No usar en niños menores de 12 años.</li>
-                                <li class="leading-normal">No use este producto si tiene problemas con los riñones.</li>
-                                <li class="leading-normal">No puede ingerir Vitamina B17 con el uso de este suplemento.</li>
+                                <li class="leading-normal">*Se recomienda que no utilice más de 2mg diarios.</li>
+                                <li class="leading-normal">*No usar en niños menores de 12 años.</li>
+                                <li class="leading-normal">*No use este producto si tiene problemas con los riñones.</li>
+                                <li class="leading-normal">*No puede ingerir Vitamina B17 con el uso de este suplemento.</li>
+                                <li class="leading-normal">*Las declaraciones sobre los suplementos dietéticos no han sido evaluadas por la FDA y no están destinadas a diagnosticar, tratar, curar o prevenir ninguna enfermedad o problema de salud.</li>
                             </ul>
-                            <p class="font-normal text-sm text-gray-700 max-w-xs leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase2.p2.dose}}</span></p>
+                            <p class="font-normal text-base text-gray-700 leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase2.p2.dose}}</span></p>
                             <!--<p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>-->
-                            <p class="font-black text-2xl text-black pb-0 pt-1">${{protocolos.phase2.p2.price}}</p>
+                            <p class="font-black text-3xl text-black pb-0 pt-1">${{protocolos.phase2.p2.price}}</p>
                         </div>
                         <!--<p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>-->
-                        <a :href="protocolos.phase2.p2.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-sm bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
+                        <a :href="protocolos.phase2.p2.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-base bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-3 py-2 lg:px-4 lg:py-3 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
                     </div>
                     <p class="w-full lg:w-4/5 text-sm mb-3 text-black mt-2 leading-tight"><span class="font-black">Importante:</span> Esta compra cubrirá un período de 120 días dentro de los 180 días sugeridos para este protocolo.</p>
                 </div>
@@ -128,17 +130,17 @@
                             <img src="../assets/wound-fixer.jpeg" class="w-24 mr-2"/>
                             <div class="mr-5">
                                 <h1 class="font-black text-base uppercase">{{protocolos.phase2.p3.name}}</h1>
-                                <p class="font-normal text-sm text-gray-700 max-w-xs leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase2.p3.dose}}</span></p>
-                                <p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>
-                                <!--<p class="font-black text-2xl text-black pb-0 pt-1">${{protocolos.phase2.p3.price}}</p>-->
+                                <p class="font-normal text-base text-gray-700 leading-tight">Dosis: <span class="font-bold text-black">{{protocolos.phase2.p3.dose}}</span></p>
+                                <!--<p class="font-black text-2xl text-graybgcdark pb-0 pt-1">NO DISPONIBLE</p>-->
+                                <p class="font-black text-3xl text-black pb-0 pt-1">${{protocolos.phase2.p3.price}}</p>
                             </div>
-                            <p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>
-                            <!--<a :href="protocolos.phase2.p3.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-sm bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>-->
+                            <!--<p class="flex flex-row lg:flex-row justify-center items-center font-normal text-sm bg-graybgcdark text-white border hover:text-white px-0 py-2 lg:px-2 lg:py-1 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-error-warning-line text-base lg:mr-2 mr-2"></i> No Disponible</p>-->
+                            <a :href="protocolos.phase2.p3.link" class="flex flex-row lg:flex-col hover:border-black justify-center items-center font-normal text-base bg-greenbgc-900 text-white border hover:bg-black hover:text-white px-3 py-2 lg:px-4 lg:py-3 ml-0 lg:ml-auto transition-all rounded-lg lg:w-auto w-full"><i class="ri-shopping-cart-2-line text-base lg:mr-0 mr-2"></i> Comprar<span class="lg:ml-0 ml-1">Individual</span></a>
                         </div>
                         <p class="w-full lg:w-4/5 text-sm mb-3 text-black mt-2 leading-tight"><span class="font-black">Importante:</span> Esta compra cubrirá un período de 30 días dentro de los {{protocolos.phase2.duration}} sugeridos para este protocolo.</p>
                     </div>
                     <!--<p class="flex flex-row flex-wrap justify-center items-center text-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-2 px-2 rounded-lg mb-5 mt-5 hover:shadow-lg transition-all font-normal text-xl focus:outline-none"><i class="ri-error-warning-line mr-3 text-xl lg:text-2xl"></i>Por el momento no esta disponible el protocolo completo, intenta comprar ImmuneBioGreenCell individualmente y recibe un 30% de descuento <br><i class="ri-home-heart-fill mr-3 text-xl lg:text-2xl"></i> #QuedateEnCasa</p>-->
-                <a :href="protocolos.phase2.linkPhase2" class="flex flex-row flex-wrap justify-center items-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-4 rounded-lg mb-5 mt-5 hover:shadow-lg hover:bg-black transition-all font-normal text-xl focus:outline-none"><i class="ri-shopping-cart-2-fill mr-3 text-xl lg:text-2xl"></i>Comprar protocolo completo<span class="ml-2 font-bold text-2xl text-center lg:text-lg lg:text-left w-full lg:w-auto lg:text-lg">${{price2()}}</span></a>
+                <a :href="protocolos.phase2.linkPhase2" class="flex flex-row flex-wrap justify-center items-center bg-red-600 w-full lg:w-4/5 mx-auto text-white py-4 rounded-lg mb-5 mt-5 hover:shadow-lg hover:bg-black transition-all font-normal text-xl focus:outline-none"><i class="ri-shopping-cart-2-fill mr-3 text-xl lg:text-2xl"></i>Comprar protocolo completo<span class="ml-2 font-bold text-2xl text-center lg:text-lg lg:text-left w-full lg:w-auto lg:text-3xl">${{price2()}}</span></a>
             </div>
             <!--Aqui finaliza la fase 2-->
         </div>
@@ -152,7 +154,9 @@
                 <li class="text-base mb-1">• Si usted toma anticoagulante por favor consúltenos antes de adquirir el producto.</li>
                 <li class="text-base mb-1">• Si usted ha sido transplantado de algún órgano, por favor consúltenos antes de adquirir el producto.</li>
                 <li class="text-base mb-1">• Este producto no está indicado para mujeres embarazadas o lactantes.</li>
-                <li class="text-base mb-1">• Consulte con su médico antes de tomar cualquier suplemento dietético, si padece alguna condición médica o si se produce alguna reacción adversa. Las declaraciones sobre los suplementos dietéticos no han sido evaluadas por la FDA y no están destinadas a diagnosticar, tratar, curar o prevenir ninguna enfermedad o problema de salud.</li>
+                <li class="text-base mb-1">• Consulte con su médico antes de tomar cualquier suplemento dietético, si padece alguna condición médica o si se produce alguna reacción adversa.</li>
+                <li class="text-base mb-1">• Las declaraciones sobre los suplementos dietéticos no han sido evaluadas por la FDA y no están destinadas a diagnosticar, tratar, curar o prevenir ninguna enfermedad o problema de salud.</li>
+                
             </ul>
         </div>
         </div>
